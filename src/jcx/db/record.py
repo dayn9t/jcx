edb851a94e011fc2833b1d996a05a8f2c0ca057c
dayn9t <dayn9t@gmail.com>
2025-07-09 10:06:49 +0800

    25-07-09

--- src/jcx/db/record.py
+++ src/jcx/db/record.py
@@ -17,3 +17,20 @@ class Record(BaseModel):
 
 type RecordFilter = Callable[[Record], bool]
 """记录过滤器"""
+
+class RecordSid(BaseModel):
+    """数据库记录"""
+
+    id: str
+    """记录ID"""
+
+    def clone(self: Self) -> Self:
+        """克隆记录"""
+        return self.model_copy(deep=True)
+
+
+type RecordFilter = Callable[[Record], bool]
+"""记录过滤器"""
+
+type RecordSidFilter = Callable[[RecordSid], bool]
+"""记录过滤器"""
